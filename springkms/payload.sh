13a74db78a8014e46b18e4776a7df57ec1c848eb
Alexi Jordanov <alexi.j@samsung.com>
2023-11-12 17:32:46 -0800

    Remove springkms, now maintained under springkmsjpa name.

--- springkms/payload.sh
+++
@@ -1,1 +0,0 @@
-curl -H "Content-type: application/json" localhost:8080/rest/v1/vehicle -d @payload.json
--- springkms/src/main/java/com/example/springkms/dao/repositories/VehicleRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.springkms.dao.repositories;
-
-import com.example.springkms.dao.entity.VehicleEntity;
-import org.springframework.data.repository.CrudRepository;
-
-public interface VehicleRepository extends CrudRepository<VehicleEntity, Long> {
-}
