de079412afeb8f53dd1d3d0ecc8ecb4be4cf7f69
Alexi Jordanov <alexi.j@samsung.com>
2023-11-24 07:35:59 -0800

    Racing conditions causes one entity ID's to be returned to another entity being inserted at the same time. MyBatis 3 handles this issue in a better way as it allows for Postgres the newly assigned ID during INSERT to be returned back to the client/driver.

--- springkmsmybatis2/src/main/java/com/example/springkmsmybatis2/dao/VehicleEntityDAO.java
+++ springkmsmybatis2/src/main/java/com/example/springkmsmybatis2/dao/VehicleEntityDAO.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.orm.ibatis.support.SqlMapClientDaoSupport;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Isolation;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
@@ -14,6 +15,9 @@ import java.util.Optional;
 
 @Repository
 @Transactional(readOnly = true)
+// Breakpoints can be set on org.springframework.orm.ibatis.SqlMapClientTemplate.execute
+// and com.ibatis.sqlmap.engine.impl.SqlMapExecutorDelegate.insert (to learn how to get
+// generated ID back using nested selectKey statement) methods.
 public class VehicleEntityDAO extends SqlMapClientDaoSupport {
 
     @Autowired
@@ -31,6 +35,13 @@ public class VehicleEntityDAO extends SqlMapClientDaoSupport {
     }
 
     @Transactional(readOnly = false)
+    // TODO: Racing conditions can cause the id of one of the inserted entity to be returned
+    // for another entity being saved at the same time. In MyBatis 3, developers can fetch
+    // the value of "RETURNING id" appended at the end of INSERT SQL statement. Did not find
+    // if this could be done in MyBatis2, which has selectKey nested in insert statement and
+    // operates differently by SELECT-ing something after the INSERT. This does not seem to
+    // work with PostgreSQL when using the sequence, and setting @Transactional's isolation
+    // to any of the possible values SERIALIZABLE, COMMITTED, UNCOMMITTED, and REPEATABLE.
     public VehicleEntity save(VehicleEntity entity) {
         Long id = (Long) getSqlMapClientTemplate().insert(
                 "com.example.springkmsmybatis2.save", entity);
